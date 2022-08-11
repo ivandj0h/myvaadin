c5d3f56c513fe2573fa8cff946b97873b8400c72
ivandjoh <ivandi.djoh@gmail.com>
2022-08-11 18:57:56 +0800

    created login, home & admin view

--- src/main/java/ivandjoh/online/myvaadin/MyVaadinApplication.java
+++ src/main/java/ivandjoh/online/myvaadin/MyVaadinApplication.java
@@ -1,12 +1,40 @@
 package ivandjoh.online.myvaadin;
 
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import ivandjoh.online.myvaadin.frontend.LoginView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
-public class MyVaadinApplication {
+public class MyVaadinApplication extends VaadinWebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(MyVaadinApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}p")
+                        .roles("ADMIN")
+                        .build(),
+                User.withUsername("user")
+                        .password("{noop}p")
+                        .roles("USER")
+                        .build()
+        );
     }
 }
---
+++ src/main/java/ivandjoh/online/myvaadin/frontend/HomeView.java
@@ -0,0 +1,15 @@
+package ivandjoh.online.myvaadin.frontend;
+
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("")
+@AnonymousAllowed
+public class HomeView extends VerticalLayout {
+
+    public HomeView() {
+        add(new H3("Home"));
+    }
+}
