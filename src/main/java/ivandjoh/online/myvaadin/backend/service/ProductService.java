601869a86f89b23ca10bd2a5bc790ec8d2682674
ivandjoh <ivandi.djoh@gmail.com>
2022-08-11 18:38:26 +0800

    updating service codes

--- src/main/java/ivandjoh/online/myvaadin/backend/service/ProductService.java
+++ src/main/java/ivandjoh/online/myvaadin/backend/service/ProductService.java
@@ -1,4 +1,12 @@
 package ivandjoh.online.myvaadin.backend.service;
 
+import ivandjoh.online.myvaadin.backend.entity.Product;
+
+import java.util.Collection;
+
 public interface ProductService {
+    public Collection<Product> findAll();
+    public Product add(Product product);
+    public Product update(Product product);
+    public void delete(Product product);
 }
